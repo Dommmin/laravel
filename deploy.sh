298bf5542c2b5753e8dc9752fead2339c6bf2a77
Dominik <d.jasinski@ideo.pl>
2024-05-24 12:37:15 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ cd /root
 sudo docker compose -f docker-compose.prod.yml down
 sudo docker compose -f docker-compose.prod.yml up -d --remove-orphans
 
-sudo docker compose -f docker-compose.prod.yml exec -T api php artisan config:cache
-sudo docker compose -f docker-compose.prod.yml exec -T api php artisan route:cache
-sudo docker compose -f docker-compose.prod.yml exec -T api php artisan view:cache
+sudo docker compose -f docker-compose.prod.yml exec -T php php artisan config:cache
+sudo docker compose -f docker-compose.prod.yml exec -T php php artisan route:cache
+sudo docker compose -f docker-compose.prod.yml exec -T php php artisan view:cache
