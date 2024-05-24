3f50430d1aa3e66a3be9df0ada2bd9a22826061e
Dominik <d.jasinski@ideo.pl>
2024-05-24 18:50:16 +0200

    feat: add frontend build and migration

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,5 @@ sudo docker compose -f docker-compose.prod.yml up -d --remove-orphans
 sudo docker compose -f docker-compose.prod.yml exec -T php php artisan config:cache
 sudo docker compose -f docker-compose.prod.yml exec -T php php artisan route:cache
 sudo docker compose -f docker-compose.prod.yml exec -T php php artisan view:cache
+
+sudo docker compose -f docker-compose.prod.yml exec -T php php artisan migrate --force
