aed63b9f144242daeac657316b80ae4363463b10
Dominik <d.jasinski@ideo.pl>
2024-05-24 21:13:49 +0200

    fix

--- docker.sh
+++ docker.sh
@@ -19,10 +19,10 @@ if [ ! -f "$ENV_FILE" ]; then
     exit 1
 fi
 
-PROJECT_ENV_FILE="$BASEDIR/api/.env"
+PROJECT_ENV_FILE="$BASEDIR/.env"
 
 if [ ! -f "$PROJECT_ENV_FILE" ]; then
-    cd api && cp .env.example .env
+    cp .env.example .env
 fi
 
 eval "$(grep ^DOCKER_PREFIX= $ENV_FILE)"
@@ -37,10 +37,10 @@ echo "${YELLOW}Uruchomienie kontenerów dla projektu ${BOLD}${DOCKER_PREFIX}${RE
 docker-compose up -d
 
 # Dodanie użytkownika, jeśli nie istnieje
-if ! docker exec -it -u root "$DOCKER_PREFIX"_api id -u $USER > /dev/null 2>&1; then
+if ! docker exec -it -u root "$DOCKER_PREFIX"_php id -u $USER > /dev/null 2>&1; then
     echo -e "${BOLD}${YELLOW}Dodanie użytkownika ${USER}${RESET}\n"
-    docker exec -it -u root "$DOCKER_PREFIX"_api addgroup -g $USER_ID $USER
-    docker exec -it -u root "$DOCKER_PREFIX"_api adduser -u $USER_ID -G $USER -h /home/$USER -D $USER
+    docker exec -it -u root "$DOCKER_PREFIX"_php addgroup -g $USER_ID $USER
+    docker exec -it -u root "$DOCKER_PREFIX"_php adduser -u $USER_ID -G $USER -h /home/$USER -D $USER
 else
     echo "${BOLD}${RED}Użytkownik ${USER} już istnieje${RESET}"
 fi
@@ -48,18 +48,18 @@ fi
 # Zmiana uprawnien
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
 echo -e "${BOLD}${YELLOW}Zmiana uprawnien${RESET}\n"
-docker exec -it -u root "$DOCKER_PREFIX"_api chown -R $USER:$USER storage
+docker exec -it -u root "$DOCKER_PREFIX"_php chown -R $USER:$USER storage
 
 # Utworzenie katalogu dla Composera i zmiana właściciela
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
 echo -e "${BOLD}${YELLOW}Utworzenie katalogu dla Composera i zmiana właściciela${RESET}\n"
-docker exec -it -u root "${DOCKER_PREFIX}_api" mkdir -p /home/$USER/.composer
-docker exec -it -u root "${DOCKER_PREFIX}_api" chown -R $USER:$USER /home/$USER
+docker exec -it -u root "${DOCKER_PREFIX}_php" mkdir -p /home/$USER/.composer
+docker exec -it -u root "${DOCKER_PREFIX}_php" chown -R $USER:$USER /home/$USER
 
 # Instalacja Composera
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
 echo -e "${BOLD}${YELLOW}Instalacja Composera${RESET}\n"
-docker exec -it -u $USER "${DOCKER_PREFIX}_api" composer install --no-scripts --no-interaction
+docker exec -it -u $USER "${DOCKER_PREFIX}_php" composer install --no-scripts --no-interaction
 
 ## Instalacja zależności npm
 #echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
@@ -69,18 +69,18 @@ docker exec -it -u $USER "${DOCKER_PREFIX}_api" composer install --no-scripts --
 # Uruchomienie migracji
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
 echo -e "${BOLD}${YELLOW}Uruchomienie migracji${RESET}\n"
-docker exec -u root "$DOCKER_PREFIX"_api bash migration.sh
+docker exec -u root "$DOCKER_PREFIX"_php bash migration.sh
 
 ## CS Fixer
 #echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
 #echo -e "${BOLD}${YELLOW}CS Fixer${RESET}\n"
-#docker exec -it -u root "$DOCKER_PREFIX"_api chown $USER:$USER ./vendor/bin/pint
-#docker exec -it "$DOCKER_PREFIX"_api composer pint
+#docker exec -it -u root "$DOCKER_PREFIX"_php chown $USER:$USER ./vendor/bin/pint
+#docker exec -it "$DOCKER_PREFIX"_php composer pint
 
 # Insights
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
 echo -e "${BOLD}${YELLOW}Insights${RESET}\n"
-docker exec -it "$DOCKER_PREFIX"_api php artisan insights --fix -n
+docker exec -it "$DOCKER_PREFIX"_php php artisan insights --fix -n
 
 # Lint
 #echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
